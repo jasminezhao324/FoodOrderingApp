3638f016cbc211512b71e8ffdfbf58baa67c81be
jasminezhao324 <52599324+jasminezhao324@users.noreply.github.com>
2021-04-26 16:26:52 +0800

    项目测试

---
+++ app/src/main/java/com/example/foodorderingapp/Activity_Merchant_Main.java
@@ -0,0 +1,16 @@
+package com.example.foodorderingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_Merchant_Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__merchant__main);
+
+
+    }
+}
